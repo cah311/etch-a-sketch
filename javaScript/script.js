ba2d410602057d58f1c6a541758f95a9f1bef290
Carson <c_halter@live.ca>
2022-04-20 05:26:25 -0600

    Take user input for height and width

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,15 +1,15 @@
 
-function makeGrid(x) {
+function makeGrid(a,b) {
     let tbl = document.getElementById("pixelCanvas")
 
-    for (let i = 0; i < x; i++) {
+    for (let i = 0; i < a; i++) {
         let myRow = document.createElement("tr");
         myRow.id = "row" + i; 
 
         tbl.appendChild(myRow);
         let rowW = document.getElementById("row" + i);
         
-        for( let j = 0; j < x; j++) {
+        for( let j = 0; j < b; j++) {
             let myCell = document.createElement("td");
 
             rowW.appendChild(myCell);
