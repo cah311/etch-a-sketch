4a024a0bf3adbe8b86fa0708d24e0577f06a7514
Carson <c_halter@live.ca>
2022-04-20 05:36:47 -0600

    Reduce grid input to 1 parameter

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,15 +1,15 @@
 
-function makeGrid(a,b) {
+function makeGrid(x) {
     let tbl = document.getElementById("pixelCanvas")
 
-    for (let i = 0; i < a; i++) {
+    for (let i = 0; i < x; i++) {
         let myRow = document.createElement("tr");
         myRow.id = "row" + i; 
 
         tbl.appendChild(myRow);
         let rowW = document.getElementById("row" + i);
         
-        for( let j = 0; j < b; j++) {
+        for( let j = 0; j < x; j++) {
             let myCell = document.createElement("td");
 
             rowW.appendChild(myCell);
