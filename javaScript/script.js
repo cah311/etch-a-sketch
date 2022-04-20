1f91fddd7810d93da0e1097a8a3a8af415177b3c
Carson <c_halter@live.ca>
2022-04-20 05:18:52 -0600

    Change hard coded grid number to variable

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,15 +1,15 @@
 
-function makeGrid() {
+function makeGrid(x) {
     let tbl = document.getElementById("pixelCanvas")
 
-    for (let i = 0; i < 11; i++) {
+    for (let i = 0; i < x; i++) {
         let myRow = document.createElement("tr");
         myRow.id = "row" + i; 
 
         tbl.appendChild(myRow);
         let rowW = document.getElementById("row" + i);
         
-        for( let j = 0; j < 11; j++) {
+        for( let j = 0; j < x; j++) {
             let myCell = document.createElement("td");
 
             rowW.appendChild(myCell);
