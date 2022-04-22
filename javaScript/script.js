e032c0a1acc859a91b4c73b0ed5fc1d0e8f40392
Carson <c_halter@live.ca>
2022-04-22 06:59:31 -0600

    Make reset btn remove rows

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,4 +1,4 @@
-
+//prompt for grid size and generate cells 
 function makeGrid() {
     
     let x = prompt("Grid Size...");
@@ -18,20 +18,14 @@ function makeGrid() {
             rowW.appendChild(myCell);
         }
     }
+    return x;
 }
+
 makeGrid();
 
 
-// document.getElementById('td2').addEventListener('mouseover', changeColor)
 
-// function changeColor(cell) {
-//     let cellColor = document.getElementById("colorPicker").value;
-    
-//     cell.target.style.backgroundColor = cellColor;
-        
-// };
-
- 
+//select color then change individual cells while mouse hover
 let table = document.querySelector('#pixelCanvas')
 let selectedCells = table.getElementsByClassName('selected');
 
@@ -46,10 +40,77 @@ table.addEventListener('mouseover', function(e) {
         selectedCells[0].className = ''
     }
     let cellColor = document.getElementById("colorPicker").value;
-    td.style.backgroundColor = cellColor;
+   
 
     
     td.className = 'selected';
-
+    td.style.backgroundColor = cellColor;
 
 })
+
+function resetGrid() {
+    
+  
+    
+    let tbl = document.getElementById("pixelCanvas")
+    let cells = document.getElementsByTagName('td')
+    
+    let cellRow = document.getElementById('row0') ;
+    
+    // console.log(cells.length)
+    
+    // console.log(cellRow)
+    // let x = "row" + 1;
+    // console.log(x)
+    // cellRow = document.getElementById(x) ;
+
+    // console.log(cellRow)
+
+
+    for (x = 0;  x <= cells.length; ) {
+        console.log(cellRow)
+        
+        
+        x++;
+        let rowNum = "row" + x;
+        cellRow = document.getElementById(rowNum);
+        console.log(cellRow)
+        tbl.removeChild(cellRow); 
+        
+    }
+    
+    // while (cells.length >= 0) {
+        
+       
+    //     tbl.removeChild(cellRow); 
+        
+        
+
+        
+    // }
+
+    
+
+    // for (k = x; k != 0; k --) {
+    //     pixelCanvas.removeChild(cellRow);
+    // }
+
+    
+
+    // let x = prompt("Grid Size...");
+
+
+    // for (let i = 0; i < x; i++) {
+    //     let myRow = document.createElement("tr");
+    //     myRow.id = "row" + i; 
+
+    //     tbl.appendChild(myRow);
+    //     let rowW = document.getElementById("row" + i);
+        
+    //     for( let j = 0; j < x; j++) {
+    //         let myCell = document.createElement("td");
+    //         myCell.id = "td" + j;
+    //         rowW.appendChild(myCell);
+    //     }
+    // }
+}
